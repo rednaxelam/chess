61f357d04ab17de4b8d389cbd03a4ef504194eeb
Michael <michael.alexander0008@gmail.com>
2024-11-21 15:06:49 +0000

    Add tests for OpponentControlInformation Rook support

--- chess/tests/OpponentControlInformation.test.js
+++ chess/tests/OpponentControlInformation.test.js
@@ -121,4 +121,116 @@ describe('OpponentControlInformation Testing', () => {
       opponentControlInformation.expectState([[6, 7], [6, 6], [4, 6], [4, 7], [3, 6]])
     })
   })
+
+  describe('Rook Effects', () => {
+
+    test('Rook marks control on empty squares', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[0, 0], [3, 3]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'white', 'test')
+      const controlCoords = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 3], [6, 3], [7, 3],
+        [3, 0], [3, 1], [3, 2], [3, 4], [3, 5], [3, 6], [3, 7]]
+      opponentControlInformation.expectState(controlCoords)
+
+      const testBoard2 = new AugmentedBoard('test')
+      testBoard2.initialiseBoard([[[7, 7], [0, 7]]])
+      const opponentControlInformation2 = new OpponentControlInformation(testBoard2, 'black', 'test')
+      const controlCoords2 = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
+        [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7]]
+      opponentControlInformation2.expectState(controlCoords2)
+    })
+
+    test('Rook marks control on unobstructed friendly pieces, but not on opposing pieces', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[0, 7], [3, 3]], [[1, 0], [5, 3]], [[6, 0], [3, 5]], [[1, 1], [3, 7]], [[1, 2], [3, 2]], [[6, 1], [2, 3]], [[1, 3], [6, 3]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'white', 'test')
+      const controlCoords = [[4, 3], [5, 3], [3, 4], [3, 2],
+        [6, 2], [6, 4], [4, 6], [4, 1], [4, 3], [7, 2], [7, 4]]
+      opponentControlInformation.expectState(controlCoords)
+    })
+
+    test('Rook stops marking control on squares in a given direction when obstructed by a piece', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[7, 0], [0, 0]], [[6, 5], [4, 0]], [[1, 7], [0, 4]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'black', 'test')
+      const controlCoords = [[1, 0], [2, 0], [3, 0], [4, 0], [0, 1], [0, 2], [0, 3],
+        [3, 1]]
+      opponentControlInformation.expectState(controlCoords)
+    })
+
+    test('Rook does not mark contol on an obstructed friendly piece', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[0, 7], [0, 0]], [[6, 0], [0, 1]], [[6, 1], [2, 0]], [[1, 0], [3, 0]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'white', 'test')
+      opponentControlInformation.expectState([[1, 0], [4, 1]])
+    })
+
+    test('Rook pins opponent pieces correctly', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[7, 0], [7, 7]], [[0, 4], [7, 5]], [[1, 0], [7, 6]], [[1, 1], [6, 7]], [[7, 7], [7, 0]], [[1, 2], [6, 0]], [[1, 3], [7, 3]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'black', 'test')
+      opponentControlInformation.expectState(
+        [[7, 1], [7, 2]],
+        'default',
+        [{ origin: [7, 0], location: [7, 3] }, { origin: [7, 7], location: [7, 6] }]
+      )
+
+      const testBoard2 = new AugmentedBoard('test')
+      testBoard2.initialiseBoard([[[6, 0], [1, 0]], [[0, 0], [0, 0]], [[6, 1], [0, 1]], [[1, 3], [0, 3]], [[7, 4], [0, 4]], [[1, 6], [0, 5]], [[0, 7], [0, 7]], [[6, 2], [1, 7]]])
+      const opponentControlInformation2 = new OpponentControlInformation(testBoard2, 'white', 'test')
+      opponentControlInformation2.expectState(
+        [[0, 5], [0, 6], [1, 2], [1, 4], [1, 6]],
+        'default',
+        []
+      )
+
+      const testBoard3 = new AugmentedBoard('test')
+      testBoard3.initialiseBoard([[[1, 0], [7, 1]], [[7, 0], [7, 0]], [[1, 1], [6, 0]], [[1, 2], [4, 0]], [[0, 4], [3, 0]], [[6, 0], [2, 0]], [[6, 1], [1, 0]], [[7, 7], [0, 0]], [[1, 3], [0, 1]]])
+      const opponentControlInformation3 = new OpponentControlInformation(testBoard3, 'black', 'test')
+      opponentControlInformation3.expectState(
+        [[1, 0], [1, 1]],
+        'default',
+        []
+      )
+
+      const testBoard4 = new AugmentedBoard('test')
+      testBoard4.initialiseBoard([[[6, 0], [7, 6]], [[0, 0], [7, 7]], [[1, 0], [5, 7]], [[6, 1], [4, 7]], [[7, 4], [2, 7]], [[0, 7], [0, 7]], [[6, 2], [0, 6]]])
+      const opponentControlInformation4 = new OpponentControlInformation(testBoard4, 'white', 'test')
+      opponentControlInformation4.expectState(
+        [[6, 7], [5, 7], [6, 6], [1, 7]],
+        createInstanceVariablesComparisonObject(true, 15, [0, 7], [2, 7], false),
+        []
+      )
+    })
+
+    test('Rook puts king in check when path to king is unobstructed, with instance variables correctly updated', () => {
+      const testBoard = new AugmentedBoard('test')
+      testBoard.initialiseBoard([[[1, 0], [6, 0]], [[7, 0], [7, 0]], [[0, 4], [7, 4]]])
+      const opponentControlInformation = new OpponentControlInformation(testBoard, 'black', 'test')
+      opponentControlInformation.expectState(
+        [[7, 1], [7, 2], [7, 3]],
+        createInstanceVariablesComparisonObject(true, 24, [7, 0], [7, 4], false),
+        []
+      )
+
+      const testBoard2 = new AugmentedBoard('test')
+      testBoard2.initialiseBoard([[[6, 0], [1, 0]], [[0, 0], [0, 0]], [[7, 4], [0, 1]]])
+      const opponentControlInformation2 = new OpponentControlInformation(testBoard2, 'white', 'test')
+      opponentControlInformation2.expectState(
+        [],
+        createInstanceVariablesComparisonObject(true, 8, [0, 0], [0, 1], false),
+        []
+      )
+
+      const testBoard3 = new AugmentedBoard('test')
+      testBoard3.initialiseBoard([[[1, 0], [6, 0]], [[7, 0], [7, 0]], [[0, 4], [7, 1]], [[7, 7], [7, 7]], [[1, 1], [6, 7]]])
+      const opponentControlInformation3 = new OpponentControlInformation(testBoard3, 'black', 'test')
+      opponentControlInformation3.expectState(
+        [[7, 6], [7, 5], [7, 4], [7, 3], [7, 2]],
+        'double',
+        []
+      )
+    })
+
+  })
 })
