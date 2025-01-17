c6ba8f71d45e6207f864d1e8658bc1b09047ba83
Michael <michael.alexander0008@gmail.com>
2025-01-17 00:04:41 +0000

    Add code to find queen moves to PlayerMovementInformation

--- chess/PlayerMovementInformation.js
+++ chess/PlayerMovementInformation.js
@@ -77,6 +77,10 @@ class PlayerMovementInformation {
         moveArray = this.#findMovesAlongMoveLines(board, startCoords, color, opponentControlInformation, moveRemovesCheck)
         this.#MoveBoard[startCoords[0]][startCoords[1]] = moveArray
         break
+      case 'queen':
+        moveArray = this.#findMovesAlongMoveLines(board, startCoords, color, opponentControlInformation, moveRemovesCheck)
+        this.#MoveBoard[startCoords[0]][startCoords[1]] = moveArray
+        break
       }
 
       if (!pieceList.hasNextPieceElement()) continueFlag = false
