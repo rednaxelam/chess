4024b9b9dcdb69badb0c78809c130ad65fa28e3f
Michael <michael.alexander0008@gmail.com>
2025-07-01 00:30:21 +0100

    Modify guest-users router to only include user id in token

--- backend/controllers/guest-users.js
+++ backend/controllers/guest-users.js
@@ -1,17 +1,15 @@
 const jwt = require('jsonwebtoken')
 const guestUsersRouter = require('express').Router()
-const { generateRandomName } = require('../utils/name-generator')
 const { v7: uuidv7 } = require('uuid')
 
 guestUsersRouter.post('/', async (request, response) => {
   const anonymousUserForToken = {
-    username: generateRandomName(),
     id: uuidv7(),
   }
 
   const token = await jwt.sign(anonymousUserForToken, process.env.SECRET)
 
-  response.status(200).send({ token, username: anonymousUserForToken.username })
+  response.status(200).send({ token })
 
 })
 
