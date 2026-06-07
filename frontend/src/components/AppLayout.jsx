16f70b8efe6839f498ca7a7b8ac74d161c21be79
Michael Alexander <88438935+rednaxelam@users.noreply.github.com>
2026-06-07 21:26:19 +0100

    Add socket initialization logic to AppLayout

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet, useNavigate } from 'react-router'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import BackButton from './BackButton'
 import User from './User'
+import initializeSocket from '../services/initializeSocket'
 
 const StyledHeader = styled.header`
   display: flex;
@@ -12,15 +13,24 @@ const StyledHeader = styled.header`
 `
 
 const AppLayout = () => {
+  // socket initialization code assumes that the layout is only mounted once
+  const startupSocketInitializationNeeded = useRef(localStorage.getItem('token') ? true : false)
   const navigate = useNavigate()
-  const onlineUserState = useSelector(state => state)
-  const isInActiveGame = onlineUserState && onlineUserState.onlineGameStatus === 2
+  const userState = useSelector(({ onlineUser }) => onlineUser)
+  const isInActiveGame = userState && userState.onlineGameStatus === 2
 
   useEffect(() => {
     if (isInActiveGame) {
       navigate('/online-game')
     }
   }, [isInActiveGame, navigate])
+
+  useEffect(() => {
+    if (startupSocketInitializationNeeded.current) {
+      initializeSocket()
+      startupSocketInitializationNeeded.current = false
+    }
+  }, [])
 
   return (
     <>
