5f8566d705880530593d429f008ec8ecf78453f2
Michael Alexander <88438935+rednaxelam@users.noreply.github.com>
2026-06-17 01:40:25 +0100

    Hide BackButton when in an active online game

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 // navigates up the site hierarchy
@@ -7,9 +8,10 @@ import styled from 'styled-components'
 const BackButton = () => {
   const navigate = useNavigate()
   const currentPathname = useLocation().pathname
-  if (currentPathname === '/') return null
+  const isGameFinished = useSelector(({ onlineGame }) => onlineGame?.gameState.gameStatus >= 4)
+  if (currentPathname === '/' || (currentPathname === '/online/active-game' && !isGameFinished)) return null
   else {
-    const parentPathname = currentPathname === '/' ? null : currentPathname.slice(0, currentPathname.lastIndexOf('/') + 1)
+    const parentPathname = currentPathname === '/' ? null : currentPathname.slice(0, currentPathname.lastIndexOf('/'))
     return <button onClick={() => navigate(parentPathname)}>◀</button>
   }
 }
