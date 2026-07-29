fa10731a4621578c2883c1d0a80941ce0875f6e4
Michael Alexander <88438935+rednaxelam@users.noreply.github.com>
2026-07-29 02:15:36 +0100

    Display piece difference instead of pieces taken, and display it for historical boards as well

--- frontend/src/components/game-ui/PlayerInfo.jsx
+++ frontend/src/components/game-ui/PlayerInfo.jsx
@@ -1,72 +1,161 @@
 import { useSelector } from 'react-redux'
 
-const calculateMaterialDiff = (whitePieceInfo, blackPieceInfo) => {
-  const pieceValues = {
-    'pawn': 1,
-    'knight': 3,
-    'bishop': 3,
-    'rook': 5,
-    'queen': 9,
-    'king': 0
+const calculateMaterialDiff = (pieceInfo, pieceInfoType) => {
+  if (pieceInfoType === 'live') {
+    const pieceValues = {
+      'pawn': 1,
+      'knight': 3,
+      'bishop': 3,
+      'rook': 5,
+      'queen': 9,
+      'king': 0
+    }
+
+    const { whitePieceInfo, blackPieceInfo } = pieceInfo
+
+    const calculateMaterialValue = (pieceInfo) => {
+      return Object.keys(pieceInfo).reduce((previousValue, key) => previousValue + (pieceValues[pieceInfo[key].type]), 0)
+    }
+
+    const whiteMaterialValue = calculateMaterialValue(whitePieceInfo)
+    const blackMaterialValue = calculateMaterialValue(blackPieceInfo)
+
+    return whiteMaterialValue - blackMaterialValue
+  } else {
+    const pieceValues = {
+      'p': 1,
+      'n': 3,
+      'b': 3,
+      'r': 5,
+      'q': 9,
+      'P': -1,
+      'N': -3,
+      'B': -3,
+      'R': -5,
+      'Q': -9,
+    }
+
+    let materialValue
+
+    if (pieceInfo === 'initial') return 0
+    else {
+      pieceInfo.forEach(char => pieceValues[char] ? materialValue += pieceValues[char] : undefined)
+      return materialValue
+    }
   }
-
-  const calculateMaterialValue = (pieceInfo) => {
-    return Object.keys(pieceInfo).reduce((previousValue, key) => previousValue + (pieceValues[pieceInfo[key].type]), 0)
-  }
-
-  const whiteMaterialValue = calculateMaterialValue(whitePieceInfo)
-  const blackMaterialValue = calculateMaterialValue(blackPieceInfo)
-
-  return whiteMaterialValue - blackMaterialValue
 }
 
-const calculatePiecesTaken = (pieceInfo, color) => {
-  const piecesTaken = {
+const calculatePieceDiff = (pieceInfo, pieceInfoType) => {
+  const pieceDiff = {
     pawn: 0,
     knight: 0,
     bishop: 0,
     rook: 0,
     queen: 0,
+    king: 0
   }
 
-  for (let id = color === 'white' ? 0 : 16; color === 'white' ? id <= 7: id <= 23; id++) {
-    if (!Object.hasOwn(pieceInfo, id) || (Object.hasOwn(pieceInfo, id) && pieceInfo[id].type !== 'pawn')) {
-      piecesTaken.pawn++
+  if (pieceInfoType === 'live') {
+    const { whitePieceInfo, blackPieceInfo } = pieceInfo
+    for (const id in whitePieceInfo) {
+      pieceDiff[whitePieceInfo[id].type]++
     }
+    for (const id in blackPieceInfo) {
+      pieceDiff[blackPieceInfo[id].type]--
+    }
+    return pieceDiff
+  } else {
+    if (pieceInfo === 'initial') return pieceDiff
+    pieceInfo.forEach(char => {
+      switch (char) {
+      case 'p':
+        pieceDiff.pawn++
+        break
+      case 'P':
+        pieceDiff.pawn--
+        break
+      case 'n':
+        pieceDiff.knight++
+        break
+      case 'N':
+        pieceDiff.knight--
+        break
+      case 'b':
+        pieceDiff.bishop++
+        break
+      case 'B':
+        pieceDiff.bishop--
+        break
+      case 'r':
+        pieceDiff.rook++
+        break
+      case 'R':
+        pieceDiff.rook--
+        break
+      case 'q':
+        pieceDiff.queen++
+        break
+      case 'Q':
+        pieceDiff.queen--
+        break
+      }
+    })
+    return pieceDiff
   }
-
-  const pieceOrder = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
-  const diff = color === 'white' ? 8 : 24
-
-  for (let i = 0; i <= 7; i++) {
-    if (!Object.hasOwn(pieceInfo, i + diff)) piecesTaken[pieceOrder[i]]++
-  }
-
-  return piecesTaken
 }
 
-const PlayerInfoDisplay = ({ color, gameState, username }) => {
-  if (!gameState || !username) return <p>loading...</p>
-
-  const { whitePieceInfo, blackPieceInfo } = gameState
-  const materialDiff = calculateMaterialDiff(whitePieceInfo, blackPieceInfo)
-  const piecesTaken = calculatePiecesTaken(color === 'white' ? blackPieceInfo : whitePieceInfo, color === 'white' ? 'black' : 'white')
-
+const PlayerInfoDisplay = ({ color, pieceInfo, pieceInfoType, username }) => {
+  const materialDiff = calculateMaterialDiff(pieceInfo, pieceInfoType)
+  const pieceDiff = calculatePieceDiff(pieceInfo, pieceInfoType)
+  const upPieces = color === 'white' ? num => num > 0 : num => num < 0
+  Object.keys(pieceDiff).forEach(type => upPieces(pieceDiff[type]) ? pieceDiff[type] = Math.abs(pieceDiff[type]) : pieceDiff[type] = 0)
   const displayMaterialDiff = (materialDiff > 0 && color === 'white') || (materialDiff < 0 && color === 'black')
   return <div>
-    <p>{username} - pawns: {piecesTaken.pawn} knights: {piecesTaken.knight} bishops: {piecesTaken.bishop} rooks: {piecesTaken.rook} queens: {piecesTaken.queen} {displayMaterialDiff && `+${Math.abs(materialDiff)}`}</p>
+    <p>{username} - pawns: {pieceDiff.pawn} knights: {pieceDiff.knight} bishops: {pieceDiff.bishop} rooks: {pieceDiff.rook} queens: {pieceDiff.queen} {displayMaterialDiff && `+${Math.abs(materialDiff)}`}</p>
   </div>
 }
 
 const LocalPlayerInfo = ({ color }) => {
   const gameState = useSelector(({ localGame }) => localGame.currentGameState)
-  return <PlayerInfoDisplay color={color} gameState={gameState} username={color} />
+  const moveHistory = useSelector(({ localGame }) => localGame.currentGameState.gameHistory.moveHistory)
+  const localPly = useSelector(({ moveHistory }) => moveHistory.localPly)
+
+  const pieceInfoType = localPly || localPly === 0 ? 'historical' : 'live'
+  let pieceInfo
+  if (pieceInfoType === 'live') {
+    pieceInfo = { whitePieceInfo: gameState.whitePieceInfo, blackPieceInfo: gameState.blackPieceInfo }
+  } else {
+    if (localPly !== -1) {
+      const plyString = moveHistory[localPly]
+      pieceInfo = plyString.split(' ')[4].split('')
+    } else {
+      pieceInfo = 'initial'
+    }
+  }
+  return <PlayerInfoDisplay color={color} pieceInfo={pieceInfo} pieceInfoType={pieceInfoType} username={color} />
 }
 
 const OnlinePlayerInfo = ({ color }) => {
   const gameState = useSelector(({ onlineGame }) => onlineGame?.gameState)
   const username = useSelector(({ onlineGame }) => onlineGame?.userState[color].username)
-  return <PlayerInfoDisplay color={color} gameState={gameState} username={username}/>
+  const moveHistory = useSelector(({ onlineGame }) => onlineGame?.gameState.gameHistory.moveHistory)
+  const onlinePly = useSelector(({ moveHistory }) => moveHistory.onlinePly)
+
+  if (!gameState || !username) return <p>loading...</p>
+
+  const pieceInfoType = onlinePly || onlinePly === 0 ? 'historical' : 'live'
+  let pieceInfo
+  if (pieceInfoType === 'live') {
+    pieceInfo = { whitePieceInfo: gameState.whitePieceInfo, blackPieceInfo: gameState.blackPieceInfo }
+  } else {
+    if (onlinePly !== -1) {
+      const plyString = moveHistory[onlinePly]
+      pieceInfo = plyString.split(' ')[4].split('')
+    } else {
+      pieceInfo = 'initial'
+    }
+  }
+  return <PlayerInfoDisplay color={color} pieceInfo={pieceInfo} pieceInfoType={pieceInfoType} username={username}/>
 }
 
 const PlayerInfo = ({ color, mode }) => {
